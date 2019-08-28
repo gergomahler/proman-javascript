e3483d11b95c2a551f4a77134d5a54673be015ad
Attila Boros <boros.attila17@gmail.com>
2019-08-28 15:07:28 +0200

    WIP empty columns

--- static/js/dom.js
+++ static/js/dom.js
@@ -34,10 +34,10 @@ export let dom = {
         let boardList = '';
 
         for(let board of boards){
-            boardList += `<section class="board">
+            boardList += `<section class="board" id="${board.id}">
                             <div class="board-header"><span class="board-title">${board.title}</span>
                                 <button class="board-add">Add Card</button>
-                                <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>
+                                <button class="board-toggle" data-board-id="${board.id}"><i class="fas fa-chevron-down"></i></button>
                             </div>
                           </section>`
         }
@@ -46,18 +46,56 @@ export let dom = {
 
         document.getElementById("boards").innerText = null;
         this._appendToElement(document.querySelector('#boards'), outerHtml);
+         let elements = document.getElementsByClassName('board-toggle');
+         for (let element of elements) {
+            element.addEventListener('click', dom.boardToggle);
+        }
     },
     loadCards: function (boardId) {
         // retrieves cards and makes showCards called
+        let cards = '';
+        dom.showCards(cards, boardId);
     },
-    showCards: function (cards) {
+    showCards: function (cards, boardId) {
         // shows the cards of a board
         // it adds necessary event listeners also
+        let columnList = `<div class="board-columns">
+                            <div class="board-column">
+                                <div class="board-column-title">New</div>
+                                <div class="board-column-content"></div>
+                            </div>
+                            <div class="board-column">
+                                <div class="board-column-title">In Progress</div>
+                                <div class="board-column-content"></div>
+                            </div>
+                            <div class="board-column">
+                                <div class="board-column-title">Testing</div>
+                                <div class="board-column-content"></div>
+                            </div>
+                            <div class="board-column">
+                                <div class="board-column-title">Done</div>
+                                <div class="board-column-content"></div>
+                            </div>
+                          </div>`;
+        columnList.toString();
+
+        this._appendToElement(document.getElementById(boardId), columnList);
+
     },
     // here comes more features
     createBoard: function () {
         let boardTitle = document.getElementById('boardName');
         dataHandler.createNewBoard(boardTitle.value, dom.loadBoards);
         document.getElementById('boardName').value = null;
+    },
+    boardToggle: function (event) {
+        let boardToggleButton = event.target;
+        let boardId = boardToggleButton.dataset.boardId;
+        if (boardToggleButton.classList.contains('fa-chevron-down')) {
+            boardToggleButton.classList.replace('fa-chevron-down', 'fa-chevron-up');
+        } else {
+            boardToggleButton.classList.replace('fa-chevron-up', 'fa-chevron-down');
+        }
+        dom.loadCards(boardId);
     }
 };
